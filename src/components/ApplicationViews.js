76c76f8ebf0c403e007bc47ad221e3f36b5cdcbf
andy.eilers <andy.eilers@asurion.com>
2022-01-26 13:09:36 -0600

    new main

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { Route } from "react-router-dom"
 import { UserList } from "./users/UserList"
-import { Feed } from "./feed/feed"
+import { Feed } from "./feed/Feed"
 
 export const ApplicationViews = () => {
   return (
