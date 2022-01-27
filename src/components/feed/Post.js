fd716fd8d176d2175efe3039b8622d63f25df85c
ChasityBrown <chasity.brown@asurion.com>
2022-01-27 15:19:46 -0500

    Done with styling for now

--- src/components/feed/Post.js
+++ src/components/feed/Post.js
@@ -3,15 +3,14 @@ import "./Post.css"
 import { Link } from "react-router-dom"
 
 export default ({ post }) => (
-    <section class="section">
-        <div class="level-item has-text-centered">
+    <section className="columns">
+        <div className="column">
             <Link to={`/posts/${post.id}`}>
                 { post.title }
             </Link>
         </div>
-        <div class="level-item has-text-centered">{ post.user.first_name }</div> 
-        <div class="level-item has-text-centered">{post.user.last_name}</div>
-        <div class="level-item has-text-centered">{post.publication_date} </div>
-        <div class="level-item has-text-centered">{post.category.label}</div>
+        <div className="column">{ post.user.first_name } {post.user.last_name}</div> 
+        <div className="column">{post.publication_date} </div>
+        <div className="column">{post.category.label}</div>
     </section>
 )
