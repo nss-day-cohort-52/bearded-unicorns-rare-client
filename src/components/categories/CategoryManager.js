e88202351abfd0e169a6ba2e8ed2a1c80c6466f1
Chris Mccrary <chris.mccrary@asurion.com>
2022-02-16 15:26:02 -0600

    Created fetch for categories, removed link tag and fetch method from CategoryList

--- src/components/categories/CategoryManager.js
+++ src/components/categories/CategoryManager.js
@@ -1,11 +1,8 @@
 export const getCategories = () => {
     return fetch("http://localhost:8000/categories", {
-        method: "GET",
         headers: {
-          "Authorization": `Token ${localStorage.getItem("lu_token")}`,
-          "Content-Type": "application/json",
-          "Accept": "application/json"
+            "Authorization": `Token ${localStorage.getItem("lu_token")}`
         }
-    }
+    })
     .then(res => res.json())
-    )}
+    }
